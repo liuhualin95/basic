d289196dd4bcab1a079b07699cff2885359d61bf
liuhualin95 <1328608834@qq.com>
2017-04-30 18:41:30 +0800

    \xA0全角空格

--- jquery源码/trim.js
+++ jquery源码/trim.js
@@ -1,7 +1,7 @@
 // \uFEFF是utf8的字节序标记，\xA0是全角空格
 rtrim = /^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g;
 core_version = "1.9.0";
-core_trim = core_version.trim;
+core_trim = core_version.trim;//相当于String.prototype.trim
 
 //core_trim可以解析全角空格的话就使用高级浏览器所支持的原生String的trim方法
 //不支持则使用正则的方法
