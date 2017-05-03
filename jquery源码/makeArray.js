2fb57353901e3fa5b942a56f3138561028f563cb
liuhualin95 <1328608834@qq.com>
2017-05-03 20:15:08 +0800

    类数组对象转化为数组对象

---
+++ jquery源码/makeArray.js
@@ -0,0 +1,14 @@
+$.makeArray = function(arr, result){
+	var res = result || [];
+	if(arr != null){
+		if(isArraylike(Object(arr))){//是类数组对象的时候
+			$.merge(arr,
+				typeof arr === "string" ? //判断是否为字符串
+				[arr] : arr
+			);
+		}else{//不是类数组对象，放到数组末尾返回
+			core_push.call(res, arr); //相当于res.push(arr)；
+		}
+	}
+	return res;
+}
