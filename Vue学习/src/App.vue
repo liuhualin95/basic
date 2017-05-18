a67b29faa90428fb64cb42637bca70648c430c55
liuhualin95 <1328608834@qq.com>
2017-05-18 22:28:47 +0800

    持续更新vue组件

---
+++ Vue学习/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <homepage-header></homepage-header>
+    <!-- <homepage-slider></homepage-slider> -->
+    <music-banner
+      :imgs="[
+        'http://p3.music.126.net/09hujI6_LSJD6DWK02qXwQ==/18964376555989719.jpg',
+        'http://p4.music.126.net/j_cH-yjz5tTLIDFmz4sSiQ==/18833534674063637.jpg',
+        'http://p4.music.126.net/I1zKnPl4g9snKbAA5zJi3Q==/18623527953173535.jpg',
+        'http://p4.music.126.net/x9KxmTxNEXHQJS9n-oWRLw==/18623527953173527.jpg',
+        'http://p3.music.126.net/U3y3VH4_3cX8pXECLFsHjA==/18624627464801303.jpg',
+        'http://p4.music.126.net/F7yr2uUq982rTIWlwvaThg==/18964376555989729.jpg',
+        'http://p4.music.126.net/qADzamZDC3c5whBibC_rAQ==/18906102439712983.jpg',
+        'http://p3.music.126.net/tw17cN9NJfa5FmFDyS0A_g==/18624627464801294.jpg'
+      ]"
+    ></music-banner>
+    <!-- <router-view></router-view> -->
+  </div>
+</template>
+
+<script>
+import './iconfont.js'
+import MusicBanner from './components/MusicBanner'
+import HomepageHeader from './components/HomepageHeader'
+import HomepageSlider from './components/HomepageSlider'
+
+
+export default {
+  components: {
+    'music-banner': MusicBanner,
+    'homepage-header': HomepageHeader,
+    'homepage-slider': HomepageSlider
+  }
+}
+</script>
+
+<style lang='scss'>
+  @import 'style/app'
+</style>
---
+++ Vue学习/src/components/HomepageHeader.vue
@@ -0,0 +1,62 @@
+<template>
+	<div class="header-container">
+		<img src="../assets/music-icon.png">
+		<label>
+			<input type="text"
+			:placeholder="placeholder">
+			<svg class="icon icon-search" aria-hidden="true">
+			  <use xlink:href="#icon-sousuo"></use>
+			</svg>
+		</label>
+		<div class="header-right">
+			<div class="user-details">
+				<!-- <div class="avatar-container"> -->
+				<img :src="avatar" class="avatar">
+				<!-- </div> -->
+				<span>{{userName}}</span>
+				<div class="small-triangle"></div>
+			</div>
+			<div class="icon-list">
+				<svg class="icon icon-skin" aria-hidden="true">
+				  <use xlink:href="#icon-skin"></use>
+				</svg>
+				<svg class="icon icon-email" aria-hidden="true">
+				  <use xlink:href="#icon-email"></use>
+				</svg>
+				<svg class="icon icon-shezhi" aria-hidden="true">
+				  <use xlink:href="#icon-shezhi"></use>
+				</svg>
+			</div>
+			<div class="another-icon-list">
+				<svg class="icon icon-minimize" aria-hidden="true">
+				  <use xlink:href="#icon-minimize"></use>
+				</svg>
+				<svg class="icon icon-minus" aria-hidden="true">
+				  <use xlink:href="#icon-minus"></use>
+				</svg>
+				<svg class="icon icon-zuidahua" aria-hidden="true">
+				  <use xlink:href="#icon-zuidahua"></use>
+				</svg>
+				<svg class="icon icon-guanbi" aria-hidden="true">
+				  <use xlink:href="#icon-guanbi"></use>
+				</svg>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+				placeholder: '搜索音乐，歌手，歌词，用户',
+				avatar: 'https://sfault-avatar.b0.upaiyun.com/147/223/147223148-573297d0913c5_huge256',
+				userName: '_小林'
+			}
+		}
+	}
+</script>
+
+<style scoped lang='scss'>
+	@import '../style/homepage-header'
+</style>
